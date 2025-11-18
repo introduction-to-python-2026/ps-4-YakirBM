d007f186f66d567b48e1a7c8395857709ead6afc
Yakir Ben Menashe-Personal <yakir.b.m.ite@gmail.com>
2025-11-18 18:32:54 +0200

    Enhance docstring with examples for split_before_each_uppercases
    Added examples to the docstring of split_before_each_uppercases function.

--- string_utils.py
+++ string_utils.py
@@ -1,11 +1,14 @@
-def split_at_first_digit(formula: str):
+def split_at_first_digit(formula: str): 
     for i, ch in enumerate(formula):
         if ch.isdigit():
             return formula[:i], int(formula[i:])
     return formula, 1
 
+###############################################################################
+
 def split_before_each_uppercases(formula: str):
     """
+    examples:
       "NaCl"     -> ["Na", "Cl"]
       "C6H12O6B" -> ["C6", "H12", "O6", "B"]
       "water"    -> ["water"]
@@ -28,3 +31,7 @@ def split_before_each_uppercases(formula: str):
     if current_sum: # we end the loop and still has
         parts.append(current_sum)
     return parts
+
+
+
+
